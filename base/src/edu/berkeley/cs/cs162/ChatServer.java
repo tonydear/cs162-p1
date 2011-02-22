4190a236d3d479b4763f5a1f27299c5b87d5d5ca
harryj <flamingfurball89@yahoo.com>
2011-02-21 20:27:53 -0800

    First real commit, adding login and user and chatgroup class

--- base/src/edu/berkeley/cs/cs162/ChatServer.java
+++ base/src/edu/berkeley/cs/cs162/ChatServer.java
@@ -1,5 +1,11 @@
 package edu.berkeley.cs.cs162;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * This is the core of the chat server.  Put the management of groups
  * and users in here.  You will need to control all of the threads,
@@ -11,9 +17,30 @@ package edu.berkeley.cs.cs162;
 
 public class ChatServer extends Thread implements ChatServerInterface {
 
+	private Map<String, User> users;
+	private Map<String, ChatGroup> groups;
+	private Set<String> allNames;
+	private ReentrantReadWriteLock lock;
+	private final static int MAX_USERS = 100;
+	
+	public ChatServer() {
+		users = new HashMap<String, User>();
+		groups = new HashMap<String, ChatGroup>();
+		allNames = new HashSet<String>();
+		lock = new ReentrantReadWriteLock();
+	}
+	
 	@Override
 	public LoginError login(String username) {
-		// TODO Auto-generated method stub
+		lock.writeLock().lock();
+		if (users.size() >= MAX_USERS) {
+			lock.writeLock().unlock();
+			return LoginError.USER_DROPPED;
+		}
+		if (allNames.contains(username)) {
+			lock.writeLock().unlock();
+			return LoginError.USER_REJECTED;
+		}
 		return null;
 	}
 
