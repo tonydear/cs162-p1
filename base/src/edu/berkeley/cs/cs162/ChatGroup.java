0ffc69573fb635c8fa3617e05aa02522ebb0aa66
Tony Dear <tonydear@berkeley.edu>
2011-02-22 20:47:06 -0800

    created ChatGroup

--- base/src/edu/berkeley/cs/cs162/ChatGroup.java
+++ base/src/edu/berkeley/cs/cs162/ChatGroup.java
@@ -1,5 +1,29 @@
 package edu.berkeley.cs.cs162;
 
-public class ChatGroup {
+import java.util.HashMap;
 
+public class ChatGroup {
+	private String name;
+	private HashMap<String, BaseUser> userlist;
+	private final static int MAX_USERS = 10;
+	
+	public boolean onCreate() {
+		
+	}
+	
+	public boolean onDelete() {
+		
+	}
+	
+	public void joinGroup(String user) {
+		
+	}
+	
+	public void leaveGroup(String user) {
+		
+	}
+	
+	public boolean forwardMessage(Message msg) {
+		
+	}
 }
