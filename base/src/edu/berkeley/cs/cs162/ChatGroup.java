ca50db067e78ce4f829b1006839db155822d2c9b
DK777 <dustin_kwong@yahoo.com>
2011-02-22 21:27:07 -0800

    ChatGroup implemented

--- base/src/edu/berkeley/cs/cs162/ChatGroup.java
+++ base/src/edu/berkeley/cs/cs162/ChatGroup.java
@@ -1,29 +1,51 @@
 package edu.berkeley.cs.cs162;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class ChatGroup {
 	private String name;
-	private HashMap<String, BaseUser> userlist;
+	private HashMap<String, User> userlist;
 	private final static int MAX_USERS = 10;
 	
+	ChatGroup(String initname) {
+		name = initname;
+		userlist = new HashMap<String, User>();
+	}
+	
 	public boolean onCreate() {
-		
+		return true;
 	}
 	
 	public boolean onDelete() {
-		
+		return true;
 	}
 	
-	public void joinGroup(String user) {
-		
+	public boolean joinGroup(String user, User userObj) {
+		if(userlist.containsKey(user))			//user already in group
+			return false;
+		if(userlist.size() + 1 > MAX_USERS)		//adding user would exceed capacity
+			return false;
+		userlist.put(user, userObj);			//add user to hashmap
+		return true;
 	}
 	
-	public void leaveGroup(String user) {
-		
+	public boolean leaveGroup(String user) {
+		if(!userlist.containsKey(user))			//user was not registered with group
+			return false;
+		userlist.remove(user);					//remove user from hashmap
+		return true;
 	}
 	
 	public boolean forwardMessage(Message msg) {
-		
+		Collection<User> users = userlist.values();
+		Iterator<User> it = users.iterator();
+		User user;
+		while(it.hasNext()) {
+			user = it.next();
+			user.msgReceived(msg);
+		}
+		return true;
 	}
 }
