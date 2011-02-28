fc8da23c9b9fd2e6a10f41533defb4f390ee952c
Jason Jee <jj41190@gmail.com>
2011-02-27 21:25:01 -0800

    added some getters to user to query for numGroups and numUsers

--- base/src/edu/berkeley/cs/cs162/User.java
+++ base/src/edu/berkeley/cs/cs162/User.java
@@ -42,6 +42,14 @@ public class User extends BaseUser {
 		// Do something with users list
 	}
 	
+	public int getNumGroups() {
+		return server.getNumGroups();
+	}
+	
+	public int getNumUsers() {
+		return server.getNumUsers();
+	}
+	
 	public List<String> getUserGroups() {
 		return groupsJoined;
 	}
