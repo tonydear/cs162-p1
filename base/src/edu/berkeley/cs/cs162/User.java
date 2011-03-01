9d23e887556dba491bc24a3a71c0c5fce85fbfcc
Tony Dear <tonydear@berkeley.edu>
2011-02-28 22:32:46 -0800

    pg 12

--- base/src/edu/berkeley/cs/cs162/User.java
+++ base/src/edu/berkeley/cs/cs162/User.java
@@ -99,7 +99,7 @@ public class User extends BaseUser {
 	public void acceptMsg(Message msg) {
 		logRecvMsg(msg);
 		TestChatServer.logUserMsgRecvd(username, msg.toString(), new Date());
-		msgReceived(msg.getSource()+"\t"+msg.getDest()+"\t"+msg.getSqn()+"\t"+msg.getContent());
+		msgReceived(msg.getSource()+"\t"+msg.getDest()+"\t"+msg.getSQN()+"\t"+msg.getContent());
 	}
 	
 	@Override
