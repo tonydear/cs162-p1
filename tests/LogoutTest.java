b87a285230106a86f346ec5c55357fed6672ad87
DK777 <dustin_kwong@yahoo.com>
2011-03-01 20:07:28 -0800

    Logout test

--- tests/LogoutTest.java
+++ tests/LogoutTest.java
@@ -33,4 +33,37 @@ public class LogoutTest {
 		assertTrue(server.logoff("A") == false);
 	}
 	
+	@Test
+	public void testLogoffWhileInGroup() {
+		System.out.println("Running logoff while in group test");
+		server.login("A");
+		server.login("B");
+		BaseUser a = server.getUser("A");
+		BaseUser b = server.getUser("B");
+		
+		//create 4 chat groups; 2 will only have a, 2 will have both
+		server.joinGroup(a, "1");
+		server.joinGroup(b, "3");
+		server.joinGroup(b, "4");
+		server.joinGroup(a, "3");
+		server.joinGroup(a, "4");
+		
+		//make sure correct number of users in group
+		assertTrue(server.getGroup("1").getNumUsers() == 1);
+		assertTrue(server.getGroup("3").getNumUsers() == 2);
+		assertTrue(server.getGroup("4").getNumUsers() == 2);
+		
+		server.logoff("A");
+		/*
+		try{
+		Thread.sleep(1000);}
+		catch(Exception e){System.out.println("Exception");}
+		*/
+		assertTrue(server.getGroup("3") == null);
+		assertTrue(server.getGroup("4") == null);
+		assertTrue(server.getGroup("1") == null);
+		System.out.println("Ran checks");
+		
+	}
+	
 }
